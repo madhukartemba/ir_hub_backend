b82c1adacbb0f0b5ccb03bfea5c7ee576269f8a9
madhukar.temba <madhukar.temba@arcadia.com>
2025-09-05 18:22:47 +0530

    enable auditing

--- src/main/java/com/madhukartemba/irhub/IrhubApplication.java
+++ src/main/java/com/madhukartemba/irhub/IrhubApplication.java
@@ -2,8 +2,10 @@ package com.madhukartemba.irhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class IrhubApplication {
 
     public static void main(String[] args) {
