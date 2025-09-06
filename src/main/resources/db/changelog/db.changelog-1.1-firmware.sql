aaa6622686a2bbb1d823839ad07ead5227a39034
madhukar.temba <madhukar.temba@arcadia.com>
2025-09-06 16:13:49 +0530

    working device schema

--- src/main/resources/db/changelog/db.changelog-1.1-firmware.sql
+++ src/main/resources/db/changelog/db.changelog-1.1-firmware.sql
@@ -27,7 +27,7 @@ CREATE TABLE Firmware (
     UNIQUE KEY uniqueFirmwareVersion (majorVersion, minorVersion)
 );
 
-CREATE TABLE DeviceVersionFirmware (
+CREATE TABLE DeviceFirmware (
     deviceVersionId CHAR(36) NOT NULL,
     firmwareId CHAR(36) NOT NULL,
     firmwareFile MEDIUMBLOB,
@@ -35,3 +35,35 @@ CREATE TABLE DeviceVersionFirmware (
     FOREIGN KEY (deviceVersionId) REFERENCES DeviceVersion(id),
     FOREIGN KEY (firmwareId) REFERENCES Firmware(id)
 );
+
+CREATE TABLE Device (
+    deviceMacId VARCHAR(255) NOT NULL,
+    deviceVersionId CHAR(36) NOT NULL,
+    firmwareId CHAR(36) NOT NULL,
+    lastSeenAt TIMESTAMP NOT NULL,
+    id CHAR(36) NOT NULL,
+    created TIMESTAMP DEFAULT NULL,
+    createdBy VARCHAR(255) DEFAULT NULL,
+    lastModified TIMESTAMP DEFAULT NULL,
+    lastModifiedBy VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY macAddress (deviceMacId),
+    FOREIGN KEY (deviceVersionId) REFERENCES DeviceVersion(id),
+    FOREIGN KEY (firmwareId) REFERENCES Firmware(id)
+);
+
+CREATE TABLE DeviceEvent (
+    deviceId CHAR(36) NOT NULL,
+    eventType VARCHAR(100) NOT NULL,
+    description VARCHAR(1024) DEFAULT NULL,
+    capturedAt TIMESTAMP NOT NULL,
+    id CHAR(36) NOT NULL,
+    created TIMESTAMP DEFAULT NULL,
+    createdBy VARCHAR(255) DEFAULT NULL,
+    lastModified TIMESTAMP DEFAULT NULL,
+    lastModifiedBy VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (deviceId) REFERENCES Device(id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_deviceevent_device_time ON DeviceEvent(deviceId, capturedAt);
